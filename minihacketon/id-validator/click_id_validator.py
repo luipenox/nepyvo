56b1d1e7a678949514837ab453fb332488532c8f
Luděk Reif <ludek.reif@gmail.com>
2020-06-08 13:43:39 +0200

    initial commit

---
+++ minihacketon/id-validator/click_id_validator.py
@@ -0,0 +1,32 @@
+import click
+from id_validator import valid_pid
+
+
+@click.command()
+@click.option('-p', '--pid', help='Name of the person to greet')
+@click.option('-i', '--info', is_flag=True, help='Name of the person to greet')
+def click_valid_pid(pid, info):
+    values = valid_pid(pid, info)
+
+    valid_text = f'--- ID {pid} is VALID ---'
+    invalid_text = f'--- ID {pid} is INVALID ---'
+
+    if info:
+        if values[0]:
+            print(valid_text)
+            print(f"Year:  {values[1]['year']:4}")
+            print(f"Month: {values[1]['month']:4}")
+            print(f"Day:   {values[1]['day']:4}")
+            print(f"Sex:   {values[1]['sex']:>4}")
+        else:
+            print(invalid_text)
+            print(f"Error: {values[1]}")
+    else:
+        if values[0]:
+            print(valid_text)
+        else:
+            print(invalid_text)
+
+
+if __name__ == '__main__':
+    click_valid_pid()

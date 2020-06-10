8c25a0acc393a3d0268c44cc07c467193b0f0884
Luděk Reif <ludek.reif@gmail.com>
2020-06-10 13:13:12 +0200

    added simple color selector code

---
+++ minihacketon/color-selector/color_selector.py
@@ -0,0 +1,36 @@
+from PyQt5 import QtWidgets, uic  # import for QMainWindow and loading designed UI
+import sys
+
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('color_selector.ui', self)
+        self.show()
+
+        # connect the button to the function
+        self.btn_show.clicked.connect(self.show_color)
+
+        # set initial text to color line edit
+        self.edit_red.setText('255')
+        self.edit_green.setText('255')
+        self.edit_blue.setText('255')
+
+    # function to read values from line edit and show the color
+    def show_color(self):
+        red = self.edit_red.text()
+        if not red:
+            red = 0
+        green = self.edit_green.text()
+        if not green:
+            green = 0
+        blue = self.edit_blue.text()
+        if not blue:
+            blue = 0
+        self.view_color.setStyleSheet(f"background-color: rgb({red}, {green}, {blue});")
+
+
+# run application
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec()
